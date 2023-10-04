6126bc67d8cadc60bcc62b5d7d2f4cb7f9ea950a
Parven <parvendranofficial05@gmail.com>
2023-10-04 18:45:45 +0800

    Adjust Arms

--- Assets/Scripts/Debug/DebugOffLight.cs
+++ Assets/Scripts/Debug/DebugOffLight.cs
@@ -1,8 +1,20 @@
+using System;
 using UnityEngine;
 
 public class DebugOffLight : MonoBehaviour
 {
     [SerializeField] private GameObject directionalLight;
+
+    private bool isLightOn = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            isLightOn = !isLightOn;
+            directionalLight.SetActive(isLightOn);
+        }
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
